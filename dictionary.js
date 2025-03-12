23b8521cda05d43875d99f1882c2775b78166f47
ozzasci <ozzasci@gmail.com>
2025-03-12 13:34:56 +0300

    Update dictionary.js

--- dictionary.js
+++ dictionary.js
@@ -1,15 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
-    fetch('words.json')  // JSON dosyanızın yolunu burada belirtin
+    fetch('words.json')
         .then(response => response.json())
         .then(data => {
+            console.log(data);  // JSON verisini kontrol etmek için
             var wordTable = document.getElementById('wordTable').getElementsByTagName('tbody')[0];
-            data.forEach(wordObj => {
-                var row = wordTable.insertRow();
-                var cell1 = row.insertCell(0);
-                var cell2 = row.insertCell(1);
-                cell1.textContent = wordObj.word;
-                cell2.textContent = wordObj.meaning;
-            });
+            if (Array.isArray(data)) {
+                data.forEach(wordObj => {
+                    var row = wordTable.insertRow();
+                    var cell1 = row.insertCell(0);
+                    var cell2 = row.insertCell(1);
+                    cell1.textContent = wordObj.word;
+                    cell2.textContent = wordObj.meaning;
+                });
+            } else {
+                console.error('JSON data is not an array:', data);
+            }
         })
         .catch(error => console.error('Error loading JSON data:', error));
 });
